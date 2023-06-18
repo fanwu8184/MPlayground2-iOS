7304cab2a52bb0586d64adcdbb27605383e395c2
Fan Wu <fanwu8184@gmail.com>
2023-06-18 14:30:20 +1000

    Initial Commit

---
+++ MPlayground2/MPlayground2App.swift
@@ -0,0 +1,17 @@
+//
+//  MPlayground2App.swift
+//  MPlayground2
+//
+//  Created by Fan Wu on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MPlayground2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
